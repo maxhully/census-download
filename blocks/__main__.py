3fd14d51d8a4e3c0d7b09877ae84a807e5a3767c
Max Hully <max.hully@gmail.com>
2019-05-11 21:58:38 -0400

    Use optparse

--- blocks/__main__.py
+++ blocks/__main__.py
@@ -1,18 +1,37 @@
 import geopandas
 import wasabi
 import uuid
+import us
 import sys
 
 from .census import block_data_for_state, block_geometries_url
 
 
-def main(state_fips, output_file=None):
-    if output_file is None:
-        output_file = f"{state_fips}_blocks.shp"
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option(
+    "-o", "--output", dest="output_file", help="save shapefile to FILE", metavar="FILE"
+)
+
+
+def main(state_fips, year=2010, output_file=None):
     printer = wasabi.Printer()
 
+    if state_fips is None:
+        printer.fail("Must specify a valid US state or territory.")
+        sys.exit(1)
+
+    printer.info("Requested state: {}.".format(us.states.lookup(state_fips).name))
+    printer.info("Tiger/LINE vintage: {}".format(year))
+
+    if output_file is None:
+        output_file = f"{state_fips}_blocks.shp"
+
+    printer.info("Output filename: {}".format(output_file))
+
     with printer.loading("Downloading geometries..."):
-        url = block_geometries_url(state_fips)
+        url = block_geometries_url(state_fips, year=year)
         gdf = geopandas.read_file(url)
     printer.good("Success!")
     with printer.loading("Downloading data..."):
@@ -39,4 +58,7 @@ def main(state_fips, output_file=None):
 
 
 if __name__ == "__main__":
-    main(*sys.argv[1:])
+    (options, args) = parser.parse_args()
+    state = args[0]
+    state_fips = us.states.lookup(state).fips
+    main(state_fips, output_file=options.output_file)
